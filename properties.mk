24581ea60fb8b8e339a8cdc01843e51685c73705
thz <diasjuremathiago@gmail.com>
2025-04-05 02:01:09 +0000

    Revert "sm6150-common: Don't force enable ims features"
    This reverts commit 0d675a6ee9d5d4c7c01440eb6cf239ad997cfec0.

--- properties.mk
+++ properties.mk
@@ -205,6 +205,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=0
 
 # IMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1  \
+    persist.dbg.wfc_avail_ovr=1
 
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.qcom.ims.use_moto_vt_ext=true
