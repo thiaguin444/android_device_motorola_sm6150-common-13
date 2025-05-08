36cd8617d58b821b9339a14b5cce48259d784ff9
thiaguin444 <thiagojurema@protonmail.com>
2025-05-08 19:13:21 -0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -246,6 +246,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.0.vendor
 
+# ConfigStore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Android Go
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
